495f1e3697faac63196d385a7833d7d1d7f578b6
Kristen Thyng <kthyng@gmail.com>
2019-05-13 15:41:30 -0500

    updated version

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@
 from distutils.core import setup
 
 setup(name='tabs',
-      version='1.0',
+      version='2.0',
       description='Read in TABS website (and other Texas) data',
       author='Kristen Thyng',
       author_email='kthyng@gmail.com',
       url='https://github.com/kthyng/tabs',
       py_modules=['tabs'],
-      install_requires=['hydrofunctions', 'pandas', 'gsw'],
+      install_requires=['hydrofunctions', 'pandas', 'gsw', 're', 'traceback'],
      )
