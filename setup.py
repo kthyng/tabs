107610ff167773885372a9ed0a9a7404dc2ac777
Kristen Thyng <kthyng@gmail.com>
2019-09-12 15:21:12 -0500

    updated version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(name='tabs-buoys',
-      version='2.1',
+      version='2.11',
       description='Read in TABS website (and other Texas) data',
       author='Kristen Thyng',
       author_email='kthyng@gmail.com',
