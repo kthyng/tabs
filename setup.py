e97ef7fe4193523740aea430d4f79f3b398a0628
Kristen Thyng <kthyng@gmail.com>
2019-12-06 09:36:37 -0600

    updated version to 2.3 after removing gsw from requirements

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@
 from setuptools import setup
 
 setup(name='tabs-buoys',
-      version='2.2',
+      version='2.3',
       description='Read in TABS website (and other Texas) data',
       author='Kristen Thyng',
       author_email='kthyng@gmail.com',
       url='https://github.com/kthyng/tabs',
       py_modules=['tabs'],
-      install_requires=['hydrofunctions', 'pandas', 'gsw'],
+      install_requires=['hydrofunctions', 'pandas'],
      )
